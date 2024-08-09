cc293b852ca51bc1a292a970e9b9b363ffca3c91
FernandoGutemberg <fernando70sil@gmail.com>
2024-08-09 10:03:13 -0300

    Criado um componente Citacao e no App.jsx inserido funcoes para trocar o nomes conforme clica no botão 'Próximo citação'

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Citacao from "./components/Citacao";
+import citacoes from "./data"; //aqui vem meus dados ficticios
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [indice, setIndice] = useState(0);
+
+
+  const proximaCitacao = () => {
+    setIndice((indiceAtual) => (indiceAtual + 1) % citacoes.length);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>{citacoes[indice].texto}</p>
+    <button className="btn btn-success mt-2" onClick={proximaCitacao}>Próxima citação</button>
+
     </>
-  )
+  );
 }
 
 export default App
